761271eb05ddcaf35136fe96ca4e5fb2443b2577
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-04-16 00:48:35 -0400

    Hi Mongo. Store data on requests.

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	MongoClient = require('mongodb').MongoClient,
+	bodyParser = require('body-parser');
 
-app.get('/', function (request, response) {
-	response.send('Hello World!');
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Initialize the connection to the Mongodb daemon.
+ *
+ * @fires `mongodb:connected` on the application.
+ */
+var openDBConnection = function(callback) {
+	var url = 'mongodb://localhost:27017/data-driven-wp';
+	MongoClient.connect(url, function (err, mongoDB) {
+		console.log("Connected correctly to server");
+		callback(mongoDB);
+	});
+}
+
+app.all( '*', function(request, response, next) {
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+	response.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+app.post('/', function (request, response, next) {
+	openDBConnection(function(mongoDB) {
+		mongoDB.collection('events').save(request.body, function (err, result) {
+			assert.equal(err, null);
+		});
+	});
 });
 
 var server = app.listen(3000, function () {
